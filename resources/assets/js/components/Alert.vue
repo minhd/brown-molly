5cd7f86716c4557895bba93b073d9246fc0fcefc
Minh Duc Nguyen <dekarvn@gmail.com>
2016-06-13 21:51:32 +1000

    Sample Vue playground

---
+++ resources/assets/js/components/Alert.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+
+    props: ['type'],
+
+    data() {
+        return {
+            show: true
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div class = "alert" v-show="show">
+        <slot></slot>
+        <span class="alert-close" @click="show = false">x</span>
+    </div>
+</template>
+
+<style>
+    .alert {
+        position:relative;
+        background: #ddd;
+        border: 1px solid darken(#ddd, 10%);
+    }
+    .alert-close {
+        position:absolute;
+        top:1em;
+        right:1em;
+        cursor:pointer;
+    }
+</style>
+
+
+
+
