ba8fcc21736c80bfd24c850106a9f5ed70a3083f
Minh Duc Nguyen <dekarvn@gmail.com>
2016-06-13 20:59:02 +1000

    use database transactions for testing tasks

--- tests/TasksTest.php
+++ tests/TasksTest.php
@@ -6,10 +6,16 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class TasksTest extends TestCase
 {
-    
+    use DatabaseTransactions;
+
+    /**
+     * Test Adding a new task without anything. Should fail
+     *
+     * @return void
+     */
 	public function testAddingTaskFail()
     {
-    	$this->json('POST', '/api/tasks', 
+    	$this->json('POST', '/api/tasks',
     		[]
 		)->seeJsonEquals([
 			"The name field is required.",
@@ -18,21 +24,24 @@ class TasksTest extends TestCase
     }
 
     /**
-     * A basic test example.
+     * Test Adding Task without user restriction fail
      *
      * @return void
      */
     public function testAddingTaskFailWithoutUser()
     {
-    	$this->json('POST', '/api/tasks', 
+    	$this->json('POST', '/api/tasks',
     		['name'=> 'stuff']
 		)->seeJsonEquals([
 			"The user id field is required."
 		]);
     }
 
-    
-
+    /**
+     * Test Adding a new Task with a correct variables
+     *
+     * @return void
+     */
     public function testAddingTaskCorrectWithUser()
     {
     	$this
